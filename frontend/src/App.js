f623e69bea2109c3c10362a60e9f13ff18159610
aldeez988 <aldeez911088@gmail.com>
2019-07-09 20:52:11 +0100

    adding fake login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
+import Login from './Login';
+import Card from './Card';
+import './App.css';
+
 const Container = styled.div``;
 
-function App() {
-  return <Container className="App">Extra Support</Container>;
+class App extends React.Component {
+  state = { loginClicked: false };
+
+  handleLogin = () => {
+    this.setState(prevStat => {
+      return {
+        loginClicked: !prevStat.loginClicked,
+      };
+    });
+  };
+  render() {
+    if (!this.state.loginClicked) {
+      return <div className="container-flex">{<Login handleLogin={this.handleLogin} />}</div>;
+    } else if (this.state.loginClicked) {
+      return (
+        <div class="cards-container">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      );
+    }
+  }
 }
 export default App;
---
+++ frontend/src/Login.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Login = props => {
+  return (
+    <form class="form-inline ">
+      <h1>Welcome To Extra Support</h1>
+      <h3>Start your search now...</h3>
+      <div className="login-form">
+        <div class="form-group">
+          <input
+            type="email"
+            class="form-control login-input"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Email"
+          />
+          <div className="student-register-container">
+            {'CYF Student - '}
+            <a className="student-volunteer-register-link">Register Here</a>
+          </div>
+        </div>
+        <div class="form-group  ml-1">
+          <input type="password" class="form-control login-input" id="exampleInputPassword1" placeholder="Password" />
+          <div className="volunteer-register-container">
+            {'Become a Volunteer - '}
+            <a className="student-volunteer-register-link">Register Here</a>
+          </div>
+        </div>
+        <button onClick={props.handleLogin} type="submit" class="btn btn-success ml-1">
+          Login{' '}
+        </button>
+      </div>
+    </form>
+  );
+};
+export default Login;
