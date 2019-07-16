8c3a82d52a61e3d5bb253e84c9bd54b76123bb05
aldeez988 <aldeez911088@gmail.com>
2019-07-16 13:56:40 +0100

    adding Nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './Routes';
+import Navbar from './Components/Nav';
 import './App.css';
 
 const App = () => (
   <Router>
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
       <div className="container">
         <Routes />
       </div>
