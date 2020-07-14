9b2c27cb28fb02614f149984797b7bb8e159bfae
Nader Akhgari <nader90akhgari@gmail.com>
2020-07-14 21:22:38 +0100

    removed unused code

--- frontend/src/Components/LoginVolunteer.js
+++ frontend/src/Components/LoginVolunteer.js
@@ -1,38 +1,32 @@
-import React from 'react'
-// import { domain } from '../../config'
-const myStyle ={
-    color: "#fcfcfc",
-    border: "3px solid #219653",
-    boxSizing: "border-box",
-    width: "50%",
-    fontWeight: "700",
-    padding: ".5rem 1rem",
-    fontSize: "1.25rem",
-    lineHeight:" 1.5",
-    borderRadius: ".3rem",
-    margin: "1%",
-    backgroundColor: "#219653",
+import React from 'react';
+const myStyle = {
+  color: '#fcfcfc',
+  border: '3px solid #219653',
+  boxSizing: 'border-box',
+  width: '50%',
+  fontWeight: '700',
+  padding: '.5rem 1rem',
+  fontSize: '1.25rem',
+  lineHeight: ' 1.5',
+  borderRadius: '.3rem',
+  margin: '1%',
+  backgroundColor: '#219653',
 };
 export default ({ loginHandler }) => (
-  
   <div>
     <h3 className="text-center mt-5">Please sign in to continue</h3>
     <div className="text-center mt-2">
-      <a
-        className="btn "
-        style={myStyle}
-        href={`localhost:3000/auth/github-admin`}
-        onClick={loginHandler}
-      >
-          <i class="fab fa-github" aria-hidden="true">Github</i>
-        {/* <i className="fab fa-github"  /> Github */}
+      <a className="btn " style={myStyle} href={`localhost:3000/auth/github-admin`} onClick={loginHandler}>
+        <i class="fab fa-github" aria-hidden="true">
+          Github
+        </i>
       </a>
     </div>
-    <div className="volunteer-register-container" style={{textAlign:"center"}}>
-    {' Become a Volunteer - '}
-              <a className="student-volunteer-register-link" href="https://codeyourfuture.io/volunteers/">
-                Register Here
-              </a>
-            </div>
+    <div className="volunteer-register-container" style={{ textAlign: 'center' }}>
+      {' Become a Volunteer - '}
+      <a className="student-volunteer-register-link" href="https://codeyourfuture.io/volunteers/">
+        Register Here
+      </a>
+    </div>
   </div>
-)
+);
