af69aeb74bfd5de477b31d53f24a6f5d451f6734
Afshin <47743758+AKaramifar@users.noreply.github.com>
2020-07-16 23:52:09 +0100

    Availabilities endpoints (Put and Delete) Done!

--- backend/src/routes/availabilities/controllers.js
+++ backend/src/routes/availabilities/controllers.js
@@ -10,44 +10,43 @@ export const getAvailabilities = async (req, res) => {
       availabilities = await AvailabilityContext.findAll();
     }
     return res.status(200).send(availabilities);
-  } catch (error) {
+  } catch (err) {
     return res.status(400).send("Could not get Availabilities");
   }
 };
 
 export const createAvailability = async (req, res) => {
-  const startDate = Date(req.body.startDate);
-  const endDate = Date(req.body.endDate);
-  const availabilityData = { ...req.body, startDate, endDate };
   try {
+    const startDate = Date(req.body.startDate);
+    const endDate = Date(req.body.endDate);
+    const availabilityData = { ...req.body, startDate, endDate };
     const availability = await AvailabilityContext.create(availabilityData);
     return res.status(200).send(availability);
   } catch (err) {
-    console.log(err);
     return res.status(400).send("Could not create your availability");
   }
 };
 
-// export const updateAvailability = async (req, res) => {
-//   const availabilityData = req.body;
-//   const _id = req.body._id;
-//   try {
-//     const availability = await AvailabilityContext.findOneAndUpdate(
-//       { _id },
-//       availabilityData
-//     );
-//     return res.status(200).send(availability);
-//   } catch (err) {
-//     return res.status(400).send("Could not update your availability");
-//   }
-// };
+export const updateAvailability = async (req, res) => {
+  try {
+    const availabilityData = req.body;
+    const _id = req.body._id;
+    const availability = await AvailabilityContext.findOneAndUpdate(
+      { _id },
+      availabilityData
+    );
+    return res.status(200).send(availability);
+  } catch (err) {
+    return res.status(400).send("Could not update your availability");
+  }
+};
 
-// export const deleteAvailability = async (req, res) => {
-//   const { _id } = req.params;
-//   try {
-//     const response = AvailabilityContext.hardDelete({ _id });
-//     return res.status(200).send(response);
-//   } catch (err) {
-//     return res.status(400).send("Could not delete the availability");
-//   }
-// };
+export const deleteAvailability = async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const response = AvailabilityContext.hardDelete({ _id });
+    return res.status(200).send(response);
+  } catch (err) {
+    return res.status(400).send("Could not delete the availability");
+  }
+};
