97eb07e2642e4dd5465ddc084ce005639b5a0269
Nader Akhgari <nader90akhgari@gmail.com>
2020-07-21 14:40:15 +0100

    if ths there is no student bookings return 'no booking found!'

--- frontend/src/Components/StudentBookings.js
+++ frontend/src/Components/StudentBookings.js
@@ -12,45 +12,49 @@ const StudentBookings = ({ studentBookings, getStudentBookings }) => {
   React.useEffect(() => {
     getStudentBookings();
   }, [getStudentBookings]);
-  return (
-    <div style={{ width: '75%' }}>
-      <br />
-      <h1>Select a category</h1>
-      <div className="categories">
-        {studentBookings.length > 0 &&
-          studentBookings.map(oneBooking => {
-            return (
-              <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <h4>{oneBooking.sesstion.title}</h4>
-                <i className="fa fa-map-marker-alt">
-                  <span className="icons">{oneBooking.sesstion.location}</span>
-                </i>
-                <i className="far fa-user">
-                  <span className="icons">
-                    {oneBooking.volunteer.firstName}
-                    {oneBooking.volunteer.lastName}
-                  </span>
-                </i>
-                <i className="fa fa-envelope">
-                  <span className="icons">
-                    {oneBooking.volunteer.email ? <span>{oneBooking.volunteer.email} </span> : null}
-                  </span>
-                </i>
-                <span>
-                  <i className="fa fa-calendar" aria-hidden="true"></i>{' '}
-                  {dayjs(oneBooking.date).format('dddd, MMMM D YYYY')}
-                </span>
-                <span>
-                  <i className="fa fa-clock-o" aria-hidden="true"></i> {oneBooking.time}
-                </span>
-              </div>
-            );
-          })}
+  if (!studentBookings.length > 0) {
+    return (
+      <div style={{ width: '75%' }}>
+        <h3 style={{ color: 'red', margin: '5% 0 ' }}>No, bookings found!</h3>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div style={{ width: '75%' }}>
+        <h1 style={{ margin: '5% 0 ' }}>Volunteer Bookings</h1>
+        <hr />
+        {studentBookings.map(oneBooking => {
+          return (
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <h4>{oneBooking.sesstion.title}</h4>
+              <i className="fa fa-map-marker-alt">
+                <span className="icons">{oneBooking.sesstion.location}</span>
+              </i>
+              <i className="far fa-user">
+                <span className="icons">
+                  {oneBooking.volunteer.firstName}
+                  {oneBooking.volunteer.lastName}
+                </span>
+              </i>
+              <i className="fa fa-envelope">
+                <span className="icons">
+                  {oneBooking.volunteer.email ? <span>{oneBooking.volunteer.email} </span> : null}
+                </span>
+              </i>
+              <span>
+                <i className="fa fa-calendar" aria-hidden="true"></i>{' '}
+                {dayjs(oneBooking.date).format('dddd, MMMM D YYYY')}
+              </span>
+              <span>
+                <i className="fa fa-clock-o" aria-hidden="true"></i> {oneBooking.time}
+              </span>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 };
-
 export default connect(
   mapStateToProps,
   { getStudentBookings }
