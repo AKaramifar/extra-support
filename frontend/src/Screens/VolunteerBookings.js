f3dc59afc4bf0fa15c39c353fdf6e39dfd2bf46a
byohannes <42332389+byohannes@users.noreply.github.com>
2020-07-21 13:54:35 +0100

    volunteers see No bookings found message when there are no bookings

--- frontend/src/Screens/VolunteerBookings.js
+++ frontend/src/Screens/VolunteerBookings.js
@@ -11,20 +11,27 @@ const VolunteerBookings = ({ volunteerBookings, getVolunteerBookings }) => {
   useEffect(() => {
     getVolunteerBookings();
   }, [getVolunteerBookings]);
-
-  return (
-    <div style={{ width: '75%' }}>
-      <h1 style={{ margin: '5% 0 ' }}>Volunteer Bookings</h1>
-      <hr />
-      <ul>
-        {volunteerBookings.map((booking, index) => (
-          <li key={index} value={booking.studentName}>
-            {(booking.studentName, booking.studentEmail, booking.bookingDate, booking.bookingTime)}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  if (!volunteerBookings.length > 0) {
+    return (
+      <div style={{ width: '75%' }}>
+        <h3 style={{ color: 'red', margin: '5% 0 ' }}>No, bookings found!</h3>
+      </div>
+    );
+  } else {
+    return (
+      <div style={{ width: '75%' }}>
+        <h1 style={{ margin: '5% 0 ' }}>Volunteer Bookings</h1>
+        <hr />
+        <ul>
+          {volunteerBookings.map((booking, index) => (
+            <li key={index} value={booking.studentName}>
+              {(booking.studentName, booking.studentEmail, booking.bookingDate, booking.bookingTime)}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default connect(
